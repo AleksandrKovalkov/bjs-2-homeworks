f6496c560891ea0032ed860827e900f4b9402e22
Aleksandr Kovalkov <akovalkoff@yandex.ru>
2023-03-27 13:16:04 +0300

    Homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,21 @@
 "use strict"
-let solveEquation = (a, b, c) => {
-  if(a == 0)
-      return false;
-  let res = {};
-  let D = b * b - 4 * a * c;
-  console.log('D = ' + D);
-  if(D < 0)
-      return false;
-  res['discriminant'] = D;
-  if(D == 0)
-      res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
-  else if(D > 0){
-      let tmp = [];
-      tmp.push((-b + Math.sqrt(D)) / (2 * a));
-      tmp.push((-b - Math.sqrt(D)) / (2 * a));
-      res["quadratic roots"] = tmp;
+function solveEquation(a, b, c) {
+  let arr =[];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  console.log('d = ' + d);
+  if (d < 0){
+    arr = [];
+  } else if 
+    (d === 0){
+      arr = [-b/(2*a)];
+  } else {
+      arr = [(-b + Math.sqrt(d) ) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a)];
   }
-  return res;
+  return arr;
 }
-console.log(solveEquation(1,12,24));
+console.log(solveEquation(1,5,4));
+console.log(solveEquation(1,2,1));
+console.log(solveEquation(1,2,10));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
@@ -33,12 +30,12 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
    		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
    	} else {
 			
-  		let today = new Date();
-  		if (today.getFullYear() > date.getFullYear()) {
-			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
+  		let today = new countMonths();
+  		if (today.getFullYear() > countMonths.getFullYear()) {
+			return `Параметр "срок ипотеки" содержит неправильное значение ${countMonths}`;
 		} else {
 			let s = am - con;						  
-  			let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); 
+  			let n = countMonths.getMonth() - today.getMonth() + (12*(countMonths.getFullYear() - today.getFullYear())); 
   			let pay = s*(per+per/(((1+per)**n)-1));			
   			let totalAmount = (pay * n).toFixed(2);
   			console.log(totalAmount);
@@ -47,4 +44,5 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     }
 
   return +totalAmount;
+
 }
