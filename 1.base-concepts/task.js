d815331eac089595d242ad91366a4677adfcb8f3
Aleksandr Kovalkov <akovalkoff@yandex.ru>
2023-03-30 10:11:45 +0300

    task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,48 +1,32 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr =[];
-  let d = Math.pow(b, 2) - 4 * a * c;
-  console.log('d = ' + d);
-  if (d < 0){
-    arr = [];
-  } else if 
-    (d === 0){
-      arr = [-b/(2*a)];
-  } else {
-      arr = [(-b + Math.sqrt(d) ) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a)];
-  }
-  return arr;
-}
-console.log(solveEquation(1,5,4));
-console.log(solveEquation(1,2,1));
-console.log(solveEquation(1,2,10));
+//function solveEquation(a, b, c) {
+//  let arr =[];
+//  let d = Math.pow(b, 2) - 4 * a * c;
+//  console.log('d = ' + d);
+//  if (d < 0){
+//    arr = [];
+//  } else if 
+//    (d === 0){
+//      arr = [-b/(2*a)];
+//  } else {
+//      arr = [(-b + Math.sqrt(d) ) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a)];
+//  }
+//return arr;
+//}
+//console.log(solveEquation(1,5,4));
+//console.log(solveEquation(1,2,1));
+//console.log(solveEquation(1,2,10));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-
-    let per = parseInt(percent) / 100 / 12; 
-    let con = parseInt(contribution); 	  	
-    let am = parseInt(amount);  			  	
-  	if (isNaN(per) || per < 0) {
-   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-   	} else if (isNaN(con) || con < 0) {
-   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-   	} else if (isNaN(am) || am < 0) {
-   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-   	} else {
-			
-  		let today = new countMonths();
-  		if (today.getFullYear() > countMonths.getFullYear()) {
-			return `Параметр "срок ипотеки" содержит неправильное значение ${countMonths}`;
-		} else {
-			let s = am - con;						  
-  			let n = countMonths.getMonth() - today.getMonth() + (12*(countMonths.getFullYear() - today.getFullYear())); 
-  			let pay = s*(per+per/(((1+per)**n)-1));			
-  			let totalAmount = (pay * n).toFixed(2);
-  			console.log(totalAmount);
- 			return +totalAmount;
- 		}	
-    }
-
-  return +totalAmount;
-
-}
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+	  return false;
+	}
+	else if (contribution === amount) {
+	  return 0;
+	}
+	let credit = amount - contribution;
+	let monthlyPercent = (percent / 100) / 12;
+	let monthlyMortgage = credit * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1)));
+	let totalMorgage = monthlyMortgage * countMonths;
+	return parseFloat(totalMorgage.toFixed(2));
+  }
